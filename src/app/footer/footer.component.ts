acb2af4c32b2d5749ab8831307ad5e1546c6b78f
zwagner <Zachary.Wagner@charter.com>
2016-10-11 11:55:32 -0500

    break header/footer out into own components

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'profile-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.less']
+})
+export class FooterComponent {}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'profile-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.less']
+})
+export class HeaderComponent {}
