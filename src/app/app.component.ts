12f4ef545e99bb6fa0f57b498d7dcf7d039672e4
zwagner <Zachary.Wagner@charter.com>
2016-10-14 16:03:50 -0500

    content and style updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hi Babes!';
+  whatDescription = "Like I said above, I have skills across the full tech stack but have developed a passion for front-end.  " +
+    "There's nothing quite like the gratification of seeing your creations in the browser and out on the web!";
 }
